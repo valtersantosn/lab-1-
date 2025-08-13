38b7da739e21fcbcc56ca03d006b7a1dd05c3ed7
valtersantosn <valtersantosn@outlook.com>
2025-08-12 22:11:47 -0300

    Create Exemplo1.c

---
+++ Exemplo1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	printf(" Mackenzie - FCI\n");
+	printf("Algoritmos e ");
+	printf(" Prog. II\n");
+	printf(" Turma - 2P ");
+	
+	//system("pause");
+	return 0;
+	
+}
